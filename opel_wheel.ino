a277e30494f767235a7587a819210b39d545832d
realthk <realthk@gmail.com>
2024-01-21 20:12:19 +0100

    averaging readout values

--- opel_wheel.ino
+++ opel_wheel.ino
@@ -1,3 +1,4 @@
+// https://raw.githubusercontent.com/dbuezas/lgt8fx/master/package_lgt8fx_index.json
 
 // On-board LED, useful for debugging
 #define LEDPIN     13 // D13
@@ -9,6 +10,8 @@
 #define ADDRESS 0x47
 
 #define OUTPUTPIN   8 // D8
+
+#define HOLDTIME 400 //Assign the time after which the button is considered clamped
 
 // remote control commands for OPEL
 const int TR_NEXT=1;
@@ -35,6 +38,8 @@ int wheelPin=A1; // steering wheel resistance reading pin
 
 int i=0;
 int prevButton=0;
+boolean flag = false;  //The flag that the previously held button is repeated
+unsigned long time;// variable for storing time
 
 void setup() {                 
   pinMode(wheelPin, INPUT);
@@ -49,65 +54,123 @@ void setup() {
 int getWheelButton() 
 { 
   // read resistance value from the steering wheel buttons
-  int r=analogRead(wheelPin);
-  //Serial.println("Read: "+r);
+  int r=getAvgValue();
   //Serial.println(r);
  
   // below values are for OPEL steering wheel controls and 470 Ohms known resistor
-  if (r>=140 && r<=180) return(VOL_DOWN);   //   85 Ohm (0.77V)
-  if (r>=240 && r<=300) return(VOL_UP);     //  170 Ohm (1.33V)
-  if (r>=350 && r<=440) return(ON_OFF);     //  290 Ohm (1.91V)
-  if (r>=460 && r<=565) return(TR_NEXT);    //  470 Ohm (2.5V)
-  if (r>=580 && r<=700) return(TR_PREV);    //  800 Ohm (3.15V)
-  if (r>=710 && r<=850) return(MODE);       // 1500 Ohm (3.81V)
+  if (r>=140 && r<=180) return(VOL_DOWN);   //   85 Ohm (0.77V) 166 is a typical value
+  if (r>=240 && r<=300) return(VOL_UP);     //  170 Ohm (1.33V) 267
+  if (r>=340 && r<=400) return(ON_OFF);     //  290 Ohm (1.91V) 345-355
+  if (r>=430 && r<=510) return(TR_NEXT);    //  470 Ohm (2.5V)  440
+  if (r>=550 && r<=650) return(TR_PREV);    //  800 Ohm (3.15V) 576
+  if (r>=670 && r<=800) return(MODE);       // 1500 Ohm (3.81V) 676
   //if (r>=900) return(0); // Error, probably broken wiring
   return (0);
 }
 
+#define NUM_READS    50
+
+int getAvgValue()
+{
+  long sum = 0;
+  int succCounter = 0; int lastRead = 0;
+  for(int i=0; i<NUM_READS; i++) {
+    int curr = analogRead(wheelPin);
+    if (i>0) {
+      int diff = abs(lastRead - curr);
+      // over 25% difference should be excluded
+      if (diff * 1000 / lastRead > 25) {
+        continue;
+      }
+    } 
+    sum += curr;
+    lastRead = curr;
+    succCounter++;
+  }
+  return sum / succCounter;
+}
 
 void loop() {
   int currButton=getWheelButton(); // get current pressed button code
   if (currButton!=prevButton) { // if it has changed since last reading
-    delay(10);
-    currButton=getWheelButton(); // wait 10ms and read again to make sure this is not just some noise
-    if (currButton!=prevButton) { 
-      //Serial.println(currButton);
-      prevButton=currButton; 
+    time = millis(); 
+    flag = false;
+    prevButton=currButton; 
 
-      // send command to JVC car stereo
+    // send command to JVC car stereo
+    switch(currButton) {
+      case VOL_UP:      
+        SendCommand(CMD_VOLUP);   
+        Serial.println("VOL_UP");
+        break;  
+
+      case VOL_DOWN:
+        SendCommand(CMD_VOLDOWN);   
+        Serial.println("VOL_DOWN");
+        break;
+
+      case TR_NEXT:
+        SendCommand(CMD_TRACKFORW);  
+        Serial.println("TR_NEXT"); 
+        break;
+
+      case TR_PREV: 
+        SendCommand(CMD_TRACKBACK);   
+        Serial.println("TR_PREV"); 
+        break;
+
+      case MODE:
+        SendCommand(CMD_FOLDERFORW);  
+        Serial.println("MODE");            
+        break;
+
+      case ON_OFF:
+        SendCommand(CMD_MUTE);   
+        Serial.println("MUTE"); 
+        break;
+    }
+  } else {
+    if (millis() >= (time + HOLDTIME) && !flag) {
       switch(currButton) {
-        case VOL_UP:      
-          SendCommand(CMD_VOLUP);   
-          Serial.println("VOL_UP");
-          break;  
-
-        case VOL_DOWN:
-          SendCommand(CMD_VOLDOWN);   
-          Serial.println("VOL_DOWN");
+        case ON_OFF:
+          SendCommand(CMD_ON_OFF);   
+          Serial.println("hold, ON_OFF"); 
           break;
 
         case TR_NEXT:
-          SendCommand(CMD_TRACKFORW);  
-          Serial.println("TR_NEXT"); 
+          SendCommand(CMD_FOLDERFORW);  
+          Serial.println("hold, FOLDER_NEXT"); 
+          delay(800);
           break;
 
         case TR_PREV: 
-          SendCommand(CMD_TRACKBACK);   
-          Serial.println("TR_PREV"); 
+          SendCommand(CMD_FOLDERBACK);   
+          Serial.println("hold, FOLDER_PREV"); 
+          delay(800);
           break;
 
+        case VOL_UP:      
+          SendCommand(CMD_VOLUP);   
+          Serial.println("hold, VOL_UP");
+          delay(150);
+          break;
+
+        case VOL_DOWN:
+          SendCommand(CMD_VOLDOWN);   
+          Serial.println("hold, VOL_DOWN");
+          delay(150);
+          break;
+          
         case MODE:
-          SendCommand(CMD_FOLDERFORW);  
-          Serial.println("MODE");            
+          SendCommand(CMD_SOURCE);   
+          Serial.println("hold, change source");
+          time = millis();  
+          flag = true; // Prevent any further repeat for this
           break;
-
-        case ON_OFF:
-          SendCommand(CMD_ON_OFF);   
-          Serial.println("ON_OFF"); 
-          break;
-     }
-   }
+      }
+    }
   }
+
   delay(5);
 }
 
